caf18c809d9b06fb817dc567064fe2e38f4cf2a2
Static Builder <innovatewithstatic@gmail.com>
2024-11-14 10:38:33 +0300

    Fixed Imports to make the app semi-functional

--- finance_tracker/src/app/_components/Header.jsx
+++ finance_tracker/src/app/_components/Header.jsx
@@ -1,28 +1,37 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { useUser, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
+import { useRouter } from "next/navigation"; // Import useRouter for redirection
+
 function Header() {
   const { user, isSignedIn } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to dashboard if signed in
+    if (isSignedIn) {
+      router.push("/dashboard");
+    }
+  }, [isSignedIn, router]);
+
   return (
     <div className="p-5 flex justify-between items-center border shadow-sm">
       <div className="flex flex-row items-center">
-        <Image src={"/moneya.svg"} alt="logo" width={40} height={25} />
-        <span className="font-bold text-xl" style={{ color: '#4CAF50' }}>
-          Finance Tracker
-        </span>
+        <Image src={"./moneya.svg"} alt="logo" width={40} height={25} />
+        <span className="font-bold text-xl" style={{ color: '#4CAF50' }}>Finance Tracker</span>
       </div>
       {isSignedIn ? (
         <UserButton />
       ) : (
-        <div className="flex gap-3  items-center">
-          <Link href={"/dashboard"}>
+        <div className="flex gap-3 items-center">
+          {/* <Link href={"/dashboard"}>
             <Button variant="outline" className="rounded-full">
               Dashboard
             </Button>
-          </Link>
+          </Link> */}
           <Link href={"/sign-in"}>
             <Button className="rounded-full">Get Started</Button>
           </Link>
@@ -33,5 +42,3 @@ function Header() {
 }
 
 export default Header;
-
-
