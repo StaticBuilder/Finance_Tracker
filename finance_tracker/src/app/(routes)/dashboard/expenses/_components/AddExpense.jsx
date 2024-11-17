1e927cc10c1afa26fd90d66a4eed8c0e4ed1e751
Static Builder <innovatewithstatic@gmail.com>
2024-11-17 17:14:14 +0300

    added type = number in the add expense file

--- finance_tracker/src/app/(routes)/dashboard/expenses/_components/AddExpense.jsx
+++ finance_tracker/src/app/(routes)/dashboard/expenses/_components/AddExpense.jsx
@@ -49,6 +49,7 @@ function AddExpense({ budgetId, user, refreshData }) {
       <div className="mt-2">
         <h2 className="text-black font-medium my-1">Expense Amount</h2>
         <Input
+          type="number"
           placeholder="e.g. 1000"
           value={amount}
           onChange={(e) => setAmount(e.target.value)}
